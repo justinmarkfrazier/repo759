d996a8ce8e93e77e7a0df96562e7048f964a4617
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-02-03 14:04:27 -0600

    Finalized plot

--- HW02/task1_plot.py
+++ HW02/task1_plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("results_euler.txt")
+data = np.loadtxt("results_euler2.txt")
 i = data[:, 0].astype(int)
 t_ms = data[:, 1]
 
@@ -12,9 +12,10 @@ plt.yscale('log')
 
 plt.grid(True, which="both", ls="--", lw=0.5)
 
+plt.title("Inclusive Scan Time vs Array Size")
 plt.xlabel("n")
 plt.ylabel("scan time (ms)")
 plt.xticks(i, [fr"$2^{{{k}}}$" for k in i])
 
 plt.tight_layout()
-plt.savefig("resultsv2_euler.png", dpi=400)
+plt.savefig("task1.pdf", dpi=400)
