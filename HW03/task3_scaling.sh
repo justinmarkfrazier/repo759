73453c600a412583720dc54059dc76f3e69a9d93
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-02-07 15:53:10 -0600

    scaling test

---
+++ HW03/task3_scaling.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+#SBATCH -p instruction
+#SBATCH --time=0-00:20:00
+#SBATCH --mem=12G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH -J hpc_hw
+
+set -euo pipefail
+
+datafile="results.txt"
+: > "$datafile"
+
+for i in {10..29}; do
+  n=$((2**i))
+
+  out=$(srun ./task3 "$n")
+
+  scan_ms=$(printf "%s\n" "$out" | head -n 1)
+
+  printf "%s %s\n" "$i" "$scan_ms" >> "$datafile"
+
+  echo "i=$i n=$n"
+  echo "$out"
+  echo
+done
+
+echo "Wrote $datafile:"
+cat "$datafile"
