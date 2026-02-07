9c979bbcf4cbcb9e8deacba49c71b1716700d527
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-02-07 12:54:59 -0600

    first task debug

--- HW03/task1.cu
+++ HW03/task1.cu
@@ -1,13 +1,32 @@
 #include <cuda.h>
-#include <stdio.h>
+#include <iostream>
 
-__global__ void simpleKernel() { printf("Hello World!\n"); }
+__global__ void simpleKernel(int* data) { 
+    // compute a! for threadIdx = a
+    int a = threadIdx.x;
+    int factorial = 1;
+    for (int i = 1; i <= a; ++i) {
+        factorial *= i;
+    }
+    data[a] = factorial;
+}
 
 int main() {
-  const int numThreads = 4;
+  const int numThreads = 8;
+  int hA[numThreads], *dA;
 
-  // invoke GPU kernel, with one block that has four threads
-  simpleKernel<<<1, numThreads>>>();
+  // allocate memory on the device (GPU); zero out all entries in this device array
+  cudaMalloc((void**)&dA, sizeof(int) * numThreads);
+  cudaMemset(dA, 0, numThreads * sizeof(int));
+  // invoke GPU kernel, with one block and numThreads threads
+  simpleKernel<<<1, numThreads>>>(dA);
+  // wait for the GPU to finish before accessing on host (CPU)
   cudaDeviceSynchronize();
+  // copy the results back to the host (CPU)
+  cudaMemcpy(hA, dA, sizeof(int) * numThreads, cudaMemcpyDeviceToHost);
+  // print the results
+    for (int i = 0; i < numThreads; ++i) {
+        std::cout << hA[i] << "\n";
+    }
   return 0;
 }
