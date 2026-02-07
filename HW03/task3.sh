8a05d6aebd8b01d5b7c02e1d61a5013deb2752bb
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-02-07 15:15:08 -0600

    debug slurm script

--- HW03/task3.sh
+++ HW03/task3.sh
@@ -9,6 +9,6 @@
 
 module load nvidia/cuda/13.0.0
 
-nvcc task3.cu vscale.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3-std=c++17 -o task3
+nvcc task3.cu vscale.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task3
 
 srun ./task3
