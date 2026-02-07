56c3003906ba2dbf261df4577f12eaae9774144c
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-02-07 15:56:13 -0600

    fixing scaling script

--- HW03/task3_scaling.sh
+++ HW03/task3_scaling.sh
@@ -14,15 +14,28 @@ datafile="results.txt"
 for i in {10..29}; do
   n=$((2**i))
 
-  out=$(srun ./task3 "$n")
+  out=$(srun --quiet ./task3 "$n")
 
-  scan_ms=$(printf "%s\n" "$out" | head -n 1)
+  # Grab the first line that *starts* with a number (handles scientific notation too),
+  # then take its first field.
+  scan_ms=$(printf "%s\n" "$out" \
+    | tr -d '\r' \
+    | awk '
+        $1 ~ /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/ { print $1; exit }
+      ')
+
+  # If parsing fails, stop with a helpful message
+  if [[ -z "${scan_ms:-}" ]]; then
+    echo "ERROR: Could not parse timing from task3 output for i=$i (n=$n)" >&2
+    echo "----- raw output -----" >&2
+    echo "$out" >&2
+    echo "----------------------" >&2
+    exit 1
+  fi
 
   printf "%s %s\n" "$i" "$scan_ms" >> "$datafile"
 
-  echo "i=$i n=$n"
-  echo "$out"
-  echo
+  echo "i=$i n=$n timing_ms=$scan_ms"
 done
 
 echo "Wrote $datafile:"
