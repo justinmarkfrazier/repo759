58264ce4de91c6140afee3f9a7f3735482a4fe1b
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-02-07 13:04:06 -0600

    debug

--- HW03/task1.cu
+++ HW03/task1.cu
@@ -1,10 +1,11 @@
 #include <cuda.h>
 #include <iostream>
+#include <stdio.h>
 
 __global__ void simpleKernel(int* data) { 
     // compute a! for threadIdx = a
     int a = threadIdx.x + 1; // threadIdx starts at 0, but we want to compute factorial starting at 1
-    std::cout << "Thread " << threadIdx.x;
+    printf("Thread %d is computing %d!\n", threadIdx.x, a);
     int factorial = 1;
     for (int i = 2; i <= a; ++i) {
         factorial *= i;
