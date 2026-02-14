8ac74ecda9cae721ce87aadaf45ff31b7c87ce03
Justin Mark Frazier <jfrazier@login35.chn.perlmutter.nersc.gov>
2026-02-14 15:16:35 -0800

    euler results

---
+++ HW04/task1_scaling_plot.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("results32_euler.txt")
+i = data[:, 0].astype(int)
+t_ms_32 = data[:, 1]
+
+data = np.loadtxt("results1024_euler.txt")
+t_ms_1024 = data[:, 1]
+
+plt.figure()
+plt.plot(i, t_ms_32, marker='o', label="32 threads")
+plt.plot(i, t_ms_1024, marker='s', label="1024 threads")
+
+plt.yscale('log')
+
+plt.grid(True, which="both", ls="--", lw=0.5)
+
+plt.title("Matrix Multiplication Time vs Array Size")
+plt.xlabel("n")
+plt.ylabel("time (ms)")
+plt.xticks(i, [fr"$2^{{{k}}}$" for k in i])
+
+plt.tight_layout()
+plt.savefig("task1.pdf", dpi=400)
