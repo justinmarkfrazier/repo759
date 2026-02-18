c6b40bb1f12168ba896ec5b6dfda96d92add1973
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-02-17 18:32:50 -0600

    plotting

---
+++ HW04/task2_scaling_plot.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("results32_t2_perlmutter.txt")
+i = data[:, 0].astype(int)
+t_ms_32 = data[:, 1]
+
+data = np.loadtxt("results1024_t2_perlmutter.txt")
+t_ms_1024 = data[:, 1]
+
+plt.figure()
+plt.plot(i, t_ms_32, marker='o', label="32 threads per block")
+plt.plot(i, t_ms_1024, marker='s', label="1024 threads per block")
+
+plt.yscale('log')
+
+plt.grid(True, which="both", ls="--", lw=0.5)
+
+plt.title("Image Convolution Time vs Array Length")
+plt.xlabel("n")
+plt.ylabel("time (ms)")
+plt.xticks(i, [fr"$2^{{{k}}}$" for k in i])
+plt.legend()
+
+plt.tight_layout()
+plt.savefig("task2.pdf", dpi=400)
