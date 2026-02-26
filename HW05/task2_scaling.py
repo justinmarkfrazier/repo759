c7b1bcc345563c5bdcee9b8752ee97997433367d
Justin Mark Frazier <jfrazier@login30.chn.perlmutter.nersc.gov>
2026-02-26 15:56:24 -0800

    task2 coding complete

--- HW05/task2_scaling.py
+++ HW05/task2_scaling.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("results2_perlmutter.txt")
+data = np.loadtxt("results2_1024.txt")
 i = data[:, 0].astype(int)
 matmul1 = data[:, 1]
 
+data = np.loadtxt("results2_256.txt")
+matmul2 = data[:, 1]
+
 plt.figure()
 plt.plot(i, matmul1, marker='o', label="1024 threads per block")
+plt.plot(i, matmul2, marker='o', label="256 threads per block")
 
 plt.yscale('log')
 
@@ -19,4 +23,4 @@ plt.xticks(i, [fr"$2^{{{k}}}$" for k in i])
 plt.legend()
 
 plt.tight_layout()
-plt.savefig("task2.pdf", dpi=400)
+plt.savefig("task2_euler.pdf", dpi=400)
