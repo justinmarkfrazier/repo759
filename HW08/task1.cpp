98e00d3aca6f7267e3716063b3fc9d1666fe0bbc
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-03-17 16:54:10 -0500

    task1 euler test

---
+++ HW08/task1.cpp
@@ -0,0 +1,61 @@
+#include "matmul.h"
+#include <iostream>
+#include <cstddef>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <type_traits>
+#include <cstdlib>
+
+// one rand_num that works for int / float / double
+template <typename T>
+T rand_num(T min, T max) {
+    static thread_local std::mt19937 rng{std::random_device{}()};
+    if constexpr (std::is_integral_v<T>) {
+        std::uniform_int_distribution<T> dist(min, max);
+        return dist(rng);
+    } else {
+        std::uniform_real_distribution<T> dist(min, max);
+        return dist(rng);
+    }
+}
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+
+int main(int argc, char *argv[])
+{
+
+    // Timing variables
+    high_resolution_clock::time_point start;
+    high_resolution_clock::time_point end;
+    duration<double, std::milli> duration_msec;
+
+    // Read command line arguments
+    const unsigned int n = std::atoi(argv[1]);
+    const unsigned int t = std::atoi(argv[2]);
+
+    // rand num range
+    const float  f_min = 0.0f, f_max = 1.0f;
+
+    // create and fill matrices A and B
+    std::vector<float> A(n*n);
+    std::vector<float> B(n*n);
+    std::vector<float> C(n * n, 0.0f);
+
+    for (unsigned int i = 0; i < n * n; ++i) {
+        A[i] = rand_num<float>(f_min, f_max);
+        B[i] = rand_num<float>(f_min, f_max);
+    }
+
+    omp_set_num_threads(t);
+
+    start = high_resolution_clock::now();
+    mmul(A.data(), B.data(), C.data(), n);
+    end = high_resolution_clock::now();
+    duration_msec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+    std::cout << C[0];
+    std::cout << "\n" << C.back();
+    std::cout << "\n" << duration_msec.count() << "\n";
+
+}
---
+++ HW08/task1_chtc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=shared
+#SBATCH --exclusive
+#SBATCH --time=0-00:10:00
+#SBATCH --mem=12G
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=20
+#SBATCH -J hpc_hw
+
+g++ task1.cpp matmul.cpp -Wall -O3 -std=c++17 -o task1 -fopenmp
+
+for t in $(seq 1 20)
+do
+    ./task1 1024 $t | awk -v t="$t" 'NR==3 {print t, $1}' >> results1.txt
+done
