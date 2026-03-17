8e30f4db4f9488bc345c4fa58db2746750503d2a
Justin Mark Frazier <jfrazier@login01.chn.perlmutter.nersc.gov>
2026-03-17 16:47:12 -0700

    task1 done

---
+++ HW08/task1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("results1.txt")
+i = data[:, 0].astype(int)
+t = data[:, 1]
+
+plt.figure()
+plt.plot(i, t, marker='o')
+
+plt.grid(True, which="both", ls="--", lw=0.5)
+
+plt.title("")
+plt.xlabel("threads")
+plt.ylabel("time (ms)")
+plt.xticks(i, [fr"{k}" for k in i])
+
+plt.tight_layout()
+plt.savefig("task1.png", dpi=400)
