9d54f24edae85b4209b126a33fa68eac9e7b377e
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-03-19 18:37:34 -0500

    fixed task1 timing

--- HW08/matmul.cpp
+++ HW08/matmul.cpp
@@ -2,9 +2,9 @@
 
 void mmul(const float* A, const float* B, float* C, const std::size_t n) {
     #pragma omp parallel for
-    for (unsigned int i = 0; i < n; ++i) {
-        for (unsigned int k = 0; k < n; ++k) {
-            for (unsigned int j = 0; j < n; ++j) {
+    for (std::size_t i = 0; i < n; ++i) {
+        for (std::size_t k = 0; k < n; ++k) {
+            for (std::size_t j = 0; j < n; ++j) {
                 C[i * n + j] += A[i * n + k] * B[k * n + j];
             }
         }
