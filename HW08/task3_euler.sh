33db3a83ca7f3cae281be26686c6d91e50881931
jmfrazier2 <jmfrazier2@spark-login.chtc.wisc.edu>
2026-03-19 19:57:20 -0500

    task3 euler test

--- HW08/task3_euler.sh
+++ HW08/task3_euler.sh
@@ -8,7 +8,7 @@
 
 g++ task3.cpp msort.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
 
-for i in $(seq 1 20)
+for i in $(seq 1 10)
 do
     ts=$((2**i))
     ./task2 1000000 8 $ts | awk -v t="$i" 'NR==3 {print t, $1}' >> results3a.txt
